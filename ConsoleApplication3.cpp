57d83bb9ffee609274470affbaf1a8e63209bbd6
OlgaKhinevich <olga-khinevich@yandex.ru>
2019-04-01 00:50:19 +0300

    work

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,50 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+#include <Windows.h>
+using namespace std;
+
+struct Dog {
+	string breed;
+	char gender;
+	string name;
+	int age;
+	string owner;
+};
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	int ages;
+	int i;
+	Dog A[5];
+	for (i = 0; i < 4; i++) {
+		cout << "Введите породу собаки: ";
+		cin >> A[i].breed;
+		cout << "Введите пол собаки: ";
+		cin >> A[i].gender;
+		cout << "Введите кличку собаки: ";
+		cin >> A[i].name;
+		cout << "Введите возраст собаки: ";
+		cin >> A[i].age;
+		cout << "Введите владельца собаки: ";
+		cin >> A[i].owner;
+		cout << '\r\n';
+	}
+	cout << "========================================" << endl;
+	cout << "Вывести собак возраста: " << endl;
+	cin >> ages;
+	for (i = 0; i <= 4; i++) {
+		if (ages == A[i].age) {
+			cout << A[i].breed + " " + A[i].name << endl;
+		}
+		else continue;
+	
+	}
+	if (i == 4) {
+		cout << "Вывести собак возраста: " << endl;
+		cin >> ages;
+	}
+}
---
+++ ConsoleApplication9.cpp
@@ -0,0 +1,62 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <Windows.h>
+using namespace std;
+
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+
+	int count = 0;
+	int size = 0;
+	int el;
+	int i;
+	vector <int> a;
+	vector <int> sizes;
+	vector <vector <int>> vec;
+	cout << "Введите количество массивов: ";
+	cin >> count;
+	for (i = 0; i < count; i++) {
+		cout << "Введите длину массива " << i + 1 << ": ";
+		cin >> size;
+		sizes.push_back(size);
+		a.clear();
+		for (int j = 0; j < size; j++)
+		{
+			cout << "Введите элемент массива " << i + 1 << ": ";
+			cin >> el;
+			a.push_back(el);
+		}
+		vec.push_back(a);
+	}
+
+	sort(sizes.begin(), sizes.end());
+
+	for (int startIndex = 0; startIndex < count; ++startIndex)
+	{
+		int smallestIndex = startIndex;
+		for (int currentIndex = startIndex + 1; currentIndex < count; ++currentIndex)
+		{
+			if (vec[currentIndex].size() < vec[smallestIndex].size())
+				smallestIndex = currentIndex;
+		}
+		swap(vec[startIndex], vec[smallestIndex]);
+	}
+
+	for (int i = 0; i < count; ++i) {
+		size = sizes[i];
+		cout << endl;
+		for (int j = 0; j < size; ++j) {
+			cout << vec[i][j] << ' ';
+		}
+	}
+	system("pause");
+}
+
+
