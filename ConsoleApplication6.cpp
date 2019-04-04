95f23d832cb58cf76ae109a930c33e53a342064a
OlgaKhinevich <olga-khinevich@yandex.ru>
2019-04-04 15:08:21 +0300

    work

---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,77 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <ctime>
+#include <Windows.h>
+using namespace std;
+
+
+class Bank_Card {
+	int Card_Number;
+	char* FIO;
+	int Account_Number;
+	int Balance;
+	int validityMonth;
+	int validityYear;
+	int PIN;
+	char* Bank_details;
+public:
+	void getInfo() {
+		cout << "Номер карты: " << Card_Number << endl;
+		cout << "ФИО владельца: " << FIO << endl;
+		cout << "Номер счёта, к которому привязана карта: " << Account_Number << endl;
+		cout << "Баланс карты: " << Balance << endl;
+		cout << "Срок действия: " << validityMonth << " " << validityYear << endl;
+		cout << "Реквизиты банка: " << Bank_details << endl;
+	}
+	void output_EUR() {
+		double Euro = Balance * 73.43;
+		cout << Euro << " евро" << endl;
+	}
+	void output_USD() {
+		double Dollar = Balance * 65.41;
+		cout << Dollar << " долларов" << endl;
+	}
+	 bool check_PIN(int &input_PIN) {
+		if (input_PIN == PIN) {
+			cout << "Пин-код введен верно" << endl;
+			return true;
+		}
+		else {
+			cout << "Пин-код не верный" << endl;
+			return false;
+		}
+	}
+	void check_validity(int &nowMonth, int &nowYear) {
+		if (validityMonth < nowMonth && validityYear < nowYear) {
+			cout << "Карта действительна" << endl;
+		}
+		else {
+			cout << "Карта недействительна" << endl;
+		}
+	}
+};
+
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	int input_PIN;
+
+	Bank_Card card1(454676879980, "Рябкина Фаина Семеновна", 459486958769789768, 128000, 12, 2021, 1234, "БИК: 123446789 ИНН: 788764543 Расчетный счет: 5465776878980 Корреспонденский счет: 565678000008766");
+	cout << "Введите пин-код: ";
+	cin >> input_PIN;
+	card1.check_PIN(input_PIN);
+	if (card1.check_PIN(input_PIN) == true)	card1.getInfo();
+
+	time_t now;
+	struct tm nowLocal;
+	now = time(NULL);
+	nowLocal = *localtime(&now);
+	int nowMonth = 1 + nowLocal.tm_mon;
+	int nowYear = 1900 + nowLocal.tm_year;
+	card1.check_validity(nowMonth, nowYear);
+	
+}
+
